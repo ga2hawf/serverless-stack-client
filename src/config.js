3bc2fd7499e6a5c40b1fb50bd3943f5eb59e181b
Anthony Hand <anthony.hand@blueyonder.co.uk>
2020-03-01 17:16:55 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "EU (London)",
+      BUCKET: "ach-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://udda7tav5i.execute-api.eu-west-2.amazonaws.com"
+    },
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_tTBr23oss",
+      APP_CLIENT_ID: "5361jesv353a2ccidveqq7lgb9",
+      IDENTITY_POOL_ID: "eu-west-2:521aee81-cbbb-4800-bc51-421ce6fe52dd"
+    }
+};
