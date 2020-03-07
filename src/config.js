cfca4cc48e67aa3ee1683482f5dbc9f24baaa841
Anthony Hand <anthony.hand@blueyonder.co.uk>
2020-03-07 15:03:02 +0000

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "EU (London)",
-      BUCKET: "ach-notes-app-uploads"
-    },
-    apiGateway: {
-      REGION: "eu-west-2",
-      URL: "https://udda7tav5i.execute-api.eu-west-2.amazonaws.com"
-    },
-    cognito: {
-      REGION: "eu-west-2",
-      USER_POOL_ID: "eu-west-2_tTBr23oss",
-      APP_CLIENT_ID: "5361jesv353a2ccidveqq7lgb9",
-      IDENTITY_POOL_ID: "eu-west-2:521aee81-cbbb-4800-bc51-421ce6fe52dd"
-    }
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_zTt3AUU1OaTmp3OLSXThLBaB00mlqceQ0V",
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "ach-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://udda7tav5i.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_tTBr23oss",
+    APP_CLIENT_ID: "5361jesv353a2ccidveqq7lgb9",
+    IDENTITY_POOL_ID: "eu-west-2:521aee81-cbbb-4800-bc51-421ce6fe52dd"
+  }
 };
